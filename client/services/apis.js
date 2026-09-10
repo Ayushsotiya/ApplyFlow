4d39d17372e94e7d12546da4ba3db3b619805a42
Ayush Sotiya <ayushsotiya56@gmail.com>
2026-09-10 14:56:58 +0530

    changing more thing for deployment

--- client/services/apis.js
+++ client/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000/api";
+const BASE_URL = process.env.BASE_URL || "http://localhost:5000/api";
 
 export const authEndpoints = {
   SENDOTP_API: `${BASE_URL}/auth/send-otp`,
@@ -14,17 +14,17 @@ export const jobEndpoints = {
   GET_JOB_BY_ID_API: `${BASE_URL}/jobs/get-by-id`,
   GET_JOB_BY_STATUS_API: `${BASE_URL}/jobs/get-jobs-by-status`,
   UPDATE_JOB_API: `${BASE_URL}/jobs/update`,
-  UPDATE_JOB_STATUS_API: `${BASE_URL}/jobs/update-status`,          // POST: { id, ...fields }
-  DELETE_JOB_API: `${BASE_URL}/jobs/delete`,            // POST: { id }
+  UPDATE_JOB_STATUS_API: `${BASE_URL}/jobs/update-status`,
+  DELETE_JOB_API: `${BASE_URL}/jobs/delete`,
   GET_DASHBOARD_API: `${BASE_URL}/jobs/dashboard`,
 };
 
 export const collectionEndpoints = {
   CREATE_COLLECTION_API: `${BASE_URL}/collections/create`,
   GET_COLLECTIONS_API: `${BASE_URL}/collections/get`,
-  GET_COLLECTION_BY_ID_API: `${BASE_URL}/collections/get-by-id`,  // POST: { id }
-  UPDATE_COLLECTION_API: `${BASE_URL}/collections/update`,         // POST: { id, name }
-  DELETE_COLLECTION_API: `${BASE_URL}/collections/delete`,         // POST: { id }
-  ADD_JOB_TO_COLLECTION_API: `${BASE_URL}/collections/add-job`,    // POST: { id, jobId }
-  REMOVE_JOB_FROM_COLLECTION_API: `${BASE_URL}/collections/remove-job`, // POST: { id, jobId }
+  GET_COLLECTION_BY_ID_API: `${BASE_URL}/collections/get-by-id`,
+  UPDATE_COLLECTION_API: `${BASE_URL}/collections/update`,
+  DELETE_COLLECTION_API: `${BASE_URL}/collections/delete`,
+  ADD_JOB_TO_COLLECTION_API: `${BASE_URL}/collections/add-job`,
+  REMOVE_JOB_FROM_COLLECTION_API: `${BASE_URL}/collections/remove-job`,
 };
