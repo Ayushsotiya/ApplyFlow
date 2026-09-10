80581e72413b2cf89f30cf2aa06e354155d88b53
Ayush Sotiya <ayushsotiya56@gmail.com>
2026-09-10 18:10:09 +0530

    modemailer issue trying

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -7,6 +7,7 @@ const mailSender = async (email, title, body) => {
             host: process.env.MAIL_HOST,
             port: 587,
             secure: false,
+            family: 4,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASSWORD,
